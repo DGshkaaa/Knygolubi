32387bd8e38a0007ad32c41f3998f8c050471667
HutsulyakDenis <dgvstup@gmail.com>
2026-09-24 16:04:41 +0300

    fix cors

--- app/main.py
+++ app/main.py
@@ -24,7 +24,10 @@ async def lifespan(_: FastAPI):
 app = FastAPI(title=settings.app_name, version="0.1.0", lifespan=lifespan)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://knygolubi-1.onrender.com"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
